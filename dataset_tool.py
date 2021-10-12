f8822b2c44b0e8e2a6d2d10b34a0410c374ac38d
Netruk44 <DanielTylerPerry@gmail.com>
2021-10-11 17:21:20 -0700

    Use fname when erroring about a file

--- dataset_tool.py
+++ dataset_tool.py
@@ -86,7 +86,7 @@ def open_image_folder(source_dir, *, max_images: Optional[int]):
             arch_fname = os.path.relpath(fname, source_dir)
             arch_fname = arch_fname.replace('\\', '/')
             img = np.array(PIL.Image.open(fname))
-            yield dict(img=img, label=labels.get(arch_fname))
+            yield dict(img=img, label=labels.get(arch_fname), fname=fname)
             if idx >= max_idx-1:
                 break
     return max_idx, iterate_images()
@@ -434,7 +434,7 @@ def convert_dataset(
                 error('Image width/height after scale and crop are required to be power-of-two')
         elif dataset_attrs != cur_image_attrs:
             err = [f'  dataset {k}/cur image {k}: {dataset_attrs[k]}/{cur_image_attrs[k]}' for k in dataset_attrs.keys()] # pylint: disable=unsubscriptable-object
-            error(f'Image {archive_fname} attributes must be equal across all images of the dataset.  Got:\n' + '\n'.join(err))
+            error(f'Image {image['fname']} attributes must be equal across all images of the dataset.  Got:\n' + '\n'.join(err))
 
         # Save the image as an uncompressed PNG.
         img = PIL.Image.fromarray(img, { 1: 'L', 3: 'RGB' }[channels])
